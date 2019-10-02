b0a7a45ec04b9ec6bf1af9461b97ca3342249e03
QuyetTV1991 <quyettv.bk@gmail.com>
2019-10-02 15:36:00 +0700

    Clean Code

--- myjs.js
+++ myjs.js
@@ -4,20 +4,20 @@ let markSienceInput = document.querySelector('.markSience')
 let markLiteratureInput = document.querySelector('.markLiterature')
 let nameInput = document.querySelector('.name')
 
-callButton.addEventListener('click', function() {
+callButton.addEventListener('click', function () {
     let name = nameInput.value
     let math = Number(markMathInput.value)
     let sience = Number(markSienceInput.value)
     let literature = Number(markLiteratureInput.value)
     let markAverage = ((math + sience + literature) / 3)
     if (math < 0 || sience < 0 || literature < 0 || math > 10 || sience > 10 || literature > 10)
-    alert('Input Illegal')
+        alert('Input Illegal')
     else if (markAverage < 5)
-    alert(name + ' is a weak student')
+        alert(name + ' is a weak student')
     else if (markAverage < 6.5)
-    alert(name + ' is an average student')
+        alert(name + ' is an average student')
     else if (markAverage < 8)
-    alert(name + ' is a good student')
+        alert(name + ' is a good student')
     else
-    alert(name + ' is an excellent student')
+        alert(name + ' is an excellent student')
 })
