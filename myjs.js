c9836a2f9a661cdaa35f33fee81da94374fb7644
QuyetTV1991 <quyettv.bk@gmail.com>
2019-10-02 14:37:07 +0700

    Development: input 3 score (math + sience + literature) error

--- myjs.js
+++ myjs.js
@@ -1,18 +1,23 @@
-let callButton = document.querySelector('.call');
-let markInput = document.querySelector('.mark');
+let callButton = document.querySelector('.call')
+let markMathInput = document.querySelector('.markMath')
+let markSienceInput = document.querySelector('.markSience')
+let markLiteratureInput = document.querySelector('.markLiterature')
 let nameInput = document.querySelector('.name')
 
 callButton.addEventListener('click', function() {
-    let name = nameInput.value;
-    let mark = markInput.value;
-    if (mark < 0 || mark > 10)
+    let name = nameInput.value
+    let math = markMathInput.value
+    let sience = markSienceInput.value
+    let literature = markLiteratureInput.value
+    let markAverage = ((math + sience + literature) / 3)
+    if (math < 0 || sience < 0 || literature < 0 || math > 10 || sience > 10 || literature > 10)
     alert('Input Illegal')
-    else if (mark < 5)
+    else if (markAverage < 5)
     alert(name + ' is a weak student')
-    else if (mark < 6.5)
+    else if (markAverage < 6.5)
     alert(name + ' is an average student')
-    else if (mark < 8)
+    else if (markAverage < 8)
     alert(name + ' is a good student')
     else
-    alert(name + 'is an excellent student')
+    alert(name + ' is an excellent student')
 })
