339565d067cada7673b071fa61b3dabf7ae48f30
QuyetTV1991 <quyettv.bk@gmail.com>
2019-10-24 16:37:37 +0700

    learnPython

---
+++ python/helloworld.py
@@ -0,0 +1,15 @@
+print 'Hello World'
+myList = ['a','b','c']
+myList.append('d')
+print 'a' in myList
+
+print 'd' in myList
+
+print 'a' not in myList
+
+print myList[-3:]
+
+print "Index for a :",myList.index('a')
+
+print "Index for d :",myList.index('d')
+
---
+++ python/operator.py
@@ -0,0 +1,15 @@
+for letter in 'Python':
+	print 'Current Letter :',letter
+
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+	print 'Current fruit :', fruit
+
+print "Good bye!"
+
+count = 0
+while (count < 9):
+	print 'The count is: ',count
+	count = count + 1
+
+print "Good bye!"
